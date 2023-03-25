5b69739d252229b41e78b0c564f80ba7f94caa66
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-03-25 15:45:18 +0600

    Конфигурацию БД

--- src/core/settings.py
+++ src/core/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cw8',
+        'USER': 'ural',
+        'PASSWORD': '1237894567oM',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
